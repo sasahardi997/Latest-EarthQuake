a5cc701bfd67b3ca6659cd079b156e84a94e645a
Aleksandar Hardi <sasahardi@gmail.com>
2020-04-29 00:01:28 +0200

    json start

---
+++ app/src/main/java/com/myappcompany/hardi/latestearthquakes/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.myappcompany.hardi.latestearthquakes.utils;
+
+public class Constants {
+
+    public static final String URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+    public static final int LIMIT = 30;
+}
