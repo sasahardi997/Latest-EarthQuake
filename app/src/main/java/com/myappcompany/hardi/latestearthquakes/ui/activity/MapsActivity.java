b442697f3372ea193224eef66d68d1c48719ac0e
Aleksandar Hardi <sasahardi@gmail.com>
2020-04-29 23:32:05 +0200

    Custom info window dialog

---
+++ app/src/main/java/com/myappcompany/hardi/latestearthquakes/ui/activity/MapsActivity.java
@@ -0,0 +1,228 @@
+package com.myappcompany.hardi.latestearthquakes.ui.activity;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.myappcompany.hardi.latestearthquakes.R;
+import com.myappcompany.hardi.latestearthquakes.model.EarthQuake;
+import com.myappcompany.hardi.latestearthquakes.ui.adapter.CustomInfoWindow;
+import com.myappcompany.hardi.latestearthquakes.utils.Constants;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleMap.OnInfoWindowClickListener, GoogleMap.OnMarkerClickListener {
+
+    private GoogleMap mMap;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    private RequestQueue queue;
+
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        progressBar = findViewById(R.id.progress_bar);
+        progressBar.setVisibility(View.VISIBLE);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        queue = Volley.newRequestQueue(this);
+        getEarthQuakes();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setInfoWindowAdapter(new CustomInfoWindow(getApplicationContext()));
+        mMap.setOnInfoWindowClickListener(this);
+        mMap.setOnMarkerClickListener(this);
+
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            //We have permission
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
+    }
+
+    public void getEarthQuakes(){
+
+        final EarthQuake earthQuake = new EarthQuake();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, Constants.URL, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray features = response.getJSONArray("features");
+                    for (int i = 0; i < Constants.LIMIT; i++){
+                        //GET properties object
+                        JSONObject properties = features.getJSONObject(i).getJSONObject("properties");
+
+                        //GET geometry object
+                        JSONObject geometry = features.getJSONObject(i).getJSONObject("geometry");
+
+                        //GET coordinates array
+                        JSONArray coordinates = geometry.getJSONArray("coordinates");
+
+                        double lon = coordinates.getDouble(0);
+                        double lat = coordinates.getDouble(0);
+
+                        //Log.d("Quake", lon + " , " + lat);
+                        earthQuake.setPlace(properties.getString("place"));
+                        earthQuake.setType(properties.getString("type"));
+                        earthQuake.setTime(properties.getLong("time"));
+                        earthQuake.setMagnitude(properties.getDouble("mag"));
+                        earthQuake.setDetail(properties.getString("detail"));
+
+                        java.text.DateFormat dateFormat = java.text.DateFormat.getDateInstance();
+                        String formattedDate = dateFormat.format(new Date(Long.valueOf(properties.getLong("time")))
+                        .getTime());
+
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+                        markerOptions.title(earthQuake.getPlace());
+                        markerOptions.position(new LatLng(lat, lon));
+                        markerOptions.snippet("Magnitude: " + earthQuake.getMagnitude() + "\n" +
+                                "Date: " + formattedDate);
+
+                        Marker marker = mMap.addMarker(markerOptions);
+                        marker.setTag(earthQuake.getDetail());
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lon), 1));
+
+                        progressBar.setVisibility(View.GONE);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(jsonObjectRequest);
+    }
+
+    private void getQuakeDetails(String url){
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                String detailsUrl = "";
+
+                try {
+                    JSONObject properties = response.getJSONObject("properties");
+                    JSONObject products = properties.getJSONObject("products");
+                    JSONArray geoserve = products.getJSONArray("geoserve");
+
+                    for (int i = 0 ; i < geoserve.length(); i++){
+                        JSONObject geoserveObj = geoserve.getJSONObject(i);
+                        JSONObject contentObj = geoserveObj.getJSONObject("contents");
+                        JSONObject geoJsonObj = contentObj.getJSONObject("geoserve.json");
+
+                        detailsUrl = geoJsonObj.getString("url");
+                    }
+                    Log.d("URL", detailsUrl);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(jsonObjectRequest);
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        getQuakeDetails(marker.getTag().toString());
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
+}
